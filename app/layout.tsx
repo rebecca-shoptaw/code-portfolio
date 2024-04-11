322ef46dc161b90142c732f2ab5d1020777f0830
Rebecca Shoptaw <rebecca.shoptaw@gmail.com>
2024-04-11 16:05:05 -0400

    Change main font and header component name

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,10 @@
 import type { Metadata } from "next";
-import { Lato } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "./(styles)/globals.css";
-import Nav from "./(components)/Nav";
+import Header from "./(components)/Header";
 
-const lato = Lato({
+const montserrat = Montserrat({
   subsets: ["latin"],
-  weight: ["100", "300", "400", "700", "900"],
 });
 
 export const metadata: Metadata = {
@@ -20,8 +19,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={lato.className}>
-        <Nav/>
+      <body className={montserrat.className}>
+        <Header />
         {children}
       </body>
     </html>
